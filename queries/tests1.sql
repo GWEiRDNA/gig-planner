b88bc50491458a974208d32f8eb3d3d4dc5dad08
D37ERTER <43070341+D37ERTER@users.noreply.github.com>
2022-02-25 01:28:40 +0100

    part 1

---
+++ queries/tests1.sql
@@ -0,0 +1,13 @@
+drop table if exists osoby cascade;
+create table osoby
+    (
+        id bigserial not null primary key,
+        imiê varchar(20) not null,
+        nazwisko varchar(50) not null,
+        data_urodzenia date not null
+    );
+
+    insert into osoby values (default, 'Zenek', 'Martyniuk', '30/4/1945');
+    insert into osoby values (default, 'Jan', 'Kaczyñski', '20/4/1889');
+
+        
